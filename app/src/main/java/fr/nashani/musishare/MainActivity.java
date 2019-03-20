5dcbd260c0b2806da8fdd9f2d73f4c6f4aa41c79
KHATIR Nassim <khatirnassim@gmail.com>
2019-03-21 00:34:59 +0100

    connection to the database

--- app/src/main/java/fr/nashani/musishare/MainActivity.java
+++ app/src/main/java/fr/nashani/musishare/MainActivity.java
@@ -36,9 +36,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
         al = new ArrayList<>();
         al.add("php");
         al.add("c");
