49f2b8c05cbaff9b9954e0e64ffc50df26c98f74
hani88 <hani@watiz.io>
2019-03-25 00:07:24 +0100

    update loginActivity design and add registration action

--- app/src/main/java/fr/nashani/musishare/LoginActivity.java
+++ app/src/main/java/fr/nashani/musishare/LoginActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,9 +20,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private Button mLogin;
+    private TextView mRegister;
     private EditText mEmail, mPassword;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+    private static void onClick(View view) {
+    }
 
     @Override
     protected void onStart() {
@@ -51,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         };
 
         mLogin = findViewById(R.id.button_submit_login);
+        mRegister = findViewById(R.id.textView_register);
         mEmail = findViewById(R.id.text_email_login);
         mPassword = findViewById(R.id.text_password_login);
 
@@ -63,6 +70,13 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
 
+        });
+
+        mRegister.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
+            startActivity(intent);
+            finish();
+            return;
         });
     }
 }
